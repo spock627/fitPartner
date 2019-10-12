f498ad35b9facf23249189ae5d06b30c8578a1b6
easy <diaoyizi@fence-labs.com>
2019-10-12 11:55:00 +0800

    ds

---
+++ src/main/java/com/gym/controller/FitController.java
@@ -0,0 +1,25 @@
+package com.gym.controller;
+
+/**
+ * business:健身任务相关业务
+ * Author:easy
+ * Date:19/10/11 下午8:15
+ */
+
+public class FitController {
+
+    //获取当前健身房的任务订单
+    public void getFitOrderList() {
+
+    }
+
+    //选择某个订单
+    public void chooseFitOrder() {
+
+    }
+
+    //获取某个订单信息
+    public void getOrderInfo() {
+
+    }
+} 
---
+++ src/main/java/com/gym/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.gym.controller;
+
+/**
+ * business:用户相关业务
+ * Author:easy
+ * Date:19/10/11 下午8:15
+ */
+
+public class UserController {
+
+    //获取用户信息
+    public void getUserInfo() {
+
+    }
+
+
+} 
